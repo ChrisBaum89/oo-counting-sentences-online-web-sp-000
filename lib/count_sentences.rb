3863258986ff92f3cf3c373c8b234237f33bf0c4
Chris Baum <christopher.baum@valpo.edu>
2021-01-24 02:22:05 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,6 +17,7 @@ class String
   def count_sentences
     split_array = []
     split_array = self.split(".")
+    split_array_2 = self.split("!")
     array_count = split_array.count
     #binding.pry
   end
